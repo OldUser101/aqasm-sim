5390d44119800f15d2fe57751a0cc807f7b3a7d6
Nathan Gill <nathan.j.gill@outlook.com>
2025-06-03 13:22:49 +0100

    Move CPU state indicators to top of pane. Use a grid layout in the register view.

--- src/cpu.tsx
+++ src/cpu.tsx
@@ -10,6 +10,10 @@ function to8BitBinary(value: number): string {
     return (value & 0xFF).toString(2).padStart(8, '0');
 }
 
+function to8BitHex(value: number): string {
+    return (value & 0xFF).toString(16).padStart(2, '0').toUpperCase();
+}
+
 function toSigned8Bit(value: number): number {
   return (value & 0x80) ? value - 0x100 : value;
 }
@@ -55,19 +59,31 @@ export function CPUState({ sim }: Props) {
     }, [sim.cpu.cmp])
 
     return (
-        <div>
-            <div className="cpu-reg-view">
-                {Array.from({ length: 13 }, (_, i) => (
-                    <p className="register-view" key={i}>R{i}: {to8BitBinary(sim.cpu.getRegister(i))} ({toSigned8Bit(sim.cpu.getRegister(i))})</p>
-                ))}
-                <p className="register-view">PC: {to8BitBinary(sim.pc)} ({sim.pc})</p>
-            </div>
+        <div className="cpu-view">
             <div className="cpu-state-view">
                 <p className="cpu-state-object cpu-state-inactive" ref={haltStateRef}>HALT</p>
                 <p className="cpu-state-object cpu-state-inactive" ref={eqStateRef}>EQ</p>
                 <p className="cpu-state-object cpu-state-inactive" ref={gtStateRef}>GT</p>
                 <p className="cpu-state-object cpu-state-inactive" ref={ltStateRef}>LT</p>
             </div>
+            <div className="cpu-reg-view">
+                {Array.from({ length: 13 }, (_, i) => (
+                    <div className="register-container" key={i}>
+                        <p>R{i}:</p>
+                        <p>{to8BitBinary(sim.cpu.getRegister(i))}</p>
+                        <p>0x{to8BitHex(sim.cpu.getRegister(i))}</p>
+                        <p>{toSigned8Bit(sim.cpu.getRegister(i))}</p>
+                    </div>
+                ))}
+                
+                <div className="register-container">
+                    <p>PC:</p>
+                    <p>{to8BitBinary(sim.pc)}</p>
+                    <p>0x{to8BitHex(sim.pc)}</p>
+                    <p>{toSigned8Bit(sim.pc)}</p>
+                </div>
+            </div>
+
         </div>
     );
 }
